e09ce92a139158d00c43a2e58411ac9e9b2aff22
unknown <hmy1304@kbu.ac.kr>
2026-06-25 10:41:27 +0900

    middleware

---
+++ src/app/components/Login.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { baseApi } from '../common/baseApi';
+import { useUserStore } from '../store/UserStore';
+
+export default function Login() {
+	const setUser = useUserStore((state) => state.setUser);
+	const setAccessToken = useUserStore((state) => state.setAccessToken);
+
+	const goLogin = async () => {
+		const res = await baseApi.post('/employees/login', {
+			email: 'hajin@naver.com',
+			password: '1234',
+		});
+
+		console.log('res >>> ', res.data.data);
+		setAccessToken(res.data.data.accessToken);
+
+		// //1.API Ppst 로그인 호츨
+		// const res = await fetch(' http://localhost:33000/api/v1/employees/login', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 	},
+		// 	body: JSON.stringify({
+		// 		email: 'hajin@naver.com',
+		// 		password: '1234',
+		// 	}),
+		// });
+
+		// //2.응답값 받아서 zustand에 저장
+		// const json = await res.json();
+		// console.log('json >> ', json);
+
+		// setUser: ({
+		// 	accessToken: json?.data?.accesstoken || '',
+		// 	email: 'tomhoon',
+		// 	departmentName: '',
+		// });
+	};
+
+	return (
+		<div className="mx-auto max-w-md rounded-lg border bg-white p-6">
+			<h1 className="mb-6 text-center text-2xl font-bold">로그인</h1>
+
+			<div className="space-y-4">
+				<input
+					type="text"
+					placeholder="아이디"
+					className="w-full rounded border px-3 py-2"
+				/>
+
+				<input
+					type="password"
+					placeholder="비밀번호"
+					className="w-full rounded border px-3 py-2"
+				/>
+
+				<button
+					className="w-full rounded bg-blue-600 py-2 text-white"
+					onClick={goLogin}
+				>
+					로그인
+				</button>
+			</div>
+		</div>
+	);
+}
